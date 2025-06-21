c42ae489048c3ea130c1ef4d8bb7c2c9389125b5
jporrego <jporregoc@gmail.com>
2025-06-21 19:57:16 -0400

    fix: separate image lists into distinct variables for improved organization and clarity in Projects component

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -6,20 +6,9 @@ interface Props {
   onImageClick: (src: string) => void;
 }
 
-const imageList = [
-  '1.jpg',
-  '2.jpg',
-  '3.jpeg',
-  '4.jpeg',
-  '5.jpg',
-  '6.jpg',
-  '7.jpeg',
-  '8.jpeg',
-  '9.jpg',
-  '10.jpg',
-  '11.jpg',
-  '12.jpg',
-];
+const imageList1 = ['1.jpg', '2.jpg', '3.jpeg', '4.jpeg', '5.jpg', '6.jpg'];
+
+const imageList2 = ['7.jpeg', '8.jpeg', '9.jpg', '10.jpg', '11.jpg', '12.jpg'];
 
 export default function Projects({ onImageClick }: Props) {
   return (
@@ -28,18 +17,36 @@ export default function Projects({ onImageClick }: Props) {
         <h2 className="mb-8 text-center text-2xl md:text-3xl">
           Proyectos destacados
         </h2>
-
+        <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:gap-4">
+          {imageList1.map((file) => {
+            const src = `/images/portfolio/${file}`;
+            return (
+              <button
+                key={src}
+                className="group relative aspect-[4/3] overflow-hidden"
+                onClick={() => onImageClick(src)}
+              >
+                <Image
+                  src={src}
+                  alt={`Mural ${file}`}
+                  fill
+                  sizes="(max-width:768px) 50vw, 33vw"
+                  className="object-cover transition-transform duration-300 group-hover:scale-105"
+                />
+              </button>
+            );
+          })}
+        </div>
         <video
           src={'/videos/color mural.mp4'}
           loop
           muted
           autoPlay
           playsInline
-          className="mb-2 w-full object-cover transition-transform duration-300"
+          className="my-2 w-full object-cover transition-transform duration-300 md:my-4"
         />
-
         <div className="grid grid-cols-2 gap-2 sm:grid-cols-3 md:gap-4">
-          {imageList.map((file) => {
+          {imageList2.map((file) => {
             const src = `/images/portfolio/${file}`;
             return (
               <button
