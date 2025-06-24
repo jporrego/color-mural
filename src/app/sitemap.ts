a825cefa7dc9a4349d7299e8fc05657e3f35bde7
jporrego <jporregoc@gmail.com>
2025-06-24 12:33:40 -0400

    feat: update sitemap lastModified date to a fixed value

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,10 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const today = new Date().toISOString();
   return [
     {
       url: 'https://www.colormural.cl',
-      lastModified: today,
+      lastModified: '2025-06-24',
     },
   ];
 }
